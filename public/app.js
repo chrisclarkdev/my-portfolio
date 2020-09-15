43a1ffa8a47fadebefddbccbc842845b1a65476b
Chrisclrk9 <chrisclrk9@gmail.com>
2020-09-15 07:16:11 +0100

    updated cards and added more info to the projects

--- public/app.js
+++ public/app.js
@@ -3,5 +3,15 @@ const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', ()=> {
     menu.classList.contains('hidden') ? menu.classList.remove('hidden') : menu.classList.add('hidden')
-  
 })
+
+// const cards = document.querySelectorAll('.card');
+// console.log(cards)
+// const hide = document.querySelector('#hide');
+
+ 
+//     document.addEventListener('click', (card)=> {
+//         hide.classList.contains('hidden') ? hide.classList.remove('hidden') : hide.classList.add('hidden')
+//         console.log(card.target);
+//     })
+
